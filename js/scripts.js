a7c1a46884a623df9e3c58813c6ca1c967600046
Jordan Meier and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-11 14:58:03 -0800

    Improve compatibility between js css and html.

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+  $(".flashcard1").click(function(){
+    $("#JavaScript").toggle();
+    $("#JavaScriptDef").toggle();
+  });
+
+  $(".flashcard2").click(function(){
+    $("#Modulo").toggle();
+    $("#ModuloDef").toggle();
+});
+
+$(".flashcard3").click(function(){
+  $("#operators").toggle();
+  $("#operatorsDef").toggle();
+});
+
+$(".flashcard4").click(function(){
+  $("#NaN").toggle();
+  $("#NaNDef").toggle();
+});
+
+$(".flashcard5").click(function(){
+  $("#VariableNaming").toggle();
+  $("#VariableNamingDef").toggle();
+});
+
+$(".flashcard6").click(function(){
+  $("#PlusEquals").toggle();
+  $("#PlusEqualsDef").toggle();
+});
+
+$(".flashcard7").click(function(){
+  $("#Function").toggle();
+  $("#FunctionDef").toggle();
+});
+
+$(".flashcard8").click(function(){
+  $("#Method").toggle();
+  $("#MethodDef").toggle();
+});
+
+$(".flashcard9").click(function(){
+  $("#Argument").toggle();
+  $("#ArgumentDef").toggle();
+});
+
+$(".flashcard10").click(function(){
+  $("#String").toggle();
+  $("#StringDef").toggle();
+});
+
+$(".flashcard11").click(function(){
+  $("#Chaining").toggle();
+  $("#ChainingDef").toggle();
+});
+
+$(".flashcard12").click(function(){
+  $("#Escape").toggle();
+  $("#EscapeDef").toggle();
+});
+
+$(".flashcard13").click(function(){
+  $("#Alert").toggle();
+  $("#AlertDef").toggle();
+});
+
+$(".flashcard14").click(function(){
+  $("#Commenting").toggle();
+  $("#CommentingDef").toggle();
+});
+
+$(".flashcard15").click(function(){
+  $("#Booleans").toggle();
+  $("#BooleansDef").toggle();
+});
+
+$(".flashcard16").click(function(){
+  $("#Undefined").toggle();
+  $("#UndefinedDef").toggle();
+});
+
+$(".flashcard17").click(function(){
+  $("#Parameter").toggle();
+  $("#ParameterDef").toggle();
+});
+
+$(".flashcard18").click(function(){
+  $("#Return").toggle();
+  $("#ReturnDef").toggle();
+});
+
+});
